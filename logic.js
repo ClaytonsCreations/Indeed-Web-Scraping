d6aa5de3db2f44c16472f9e3400bc3d0e290b207
Eamado18 <89824082+Eamado18@users.noreply.github.com>
2022-01-22 00:35:42 -0800

    Add files via upload

---
+++ logic.js
@@ -0,0 +1,19 @@
+var myMap = L.map("map", {
+  center: [33.39, -118.42],
+  zoom: 11
+});
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(myMap);  
+
+var markers = L.markerClusterGroup()
+
+d3.csv("Indeed_coordinates.csv", function(data) {
+  var lat = data.lat
+  var lng = data.lng
+  markers.addLayer(L.marker([lat, lng]).bindPopup("Job Title: "  + data.jobtitle + "<br>Company: " + data.company));
+
+  myMap.addLayer(markers);
+ 
+});
